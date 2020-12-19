41005e880b8ee8d97e48b99090563fc95ec9a80b
ameliepcrd <49780805+ameliepcrd@users.noreply.github.com>
2020-12-19 14:52:15 +0100

    Add files via upload

---
+++ chargement_bank.R
@@ -0,0 +1,12 @@
+#Réseau de neurone à une couche caché ! 
+
+##chargement des données
+setwd("C:/Users/ameli/Desktop/R/data_mining")
+
+data<-read.csv2("bank.csv")
+
+class(data)
+str(data)
+head(data)
+summary(data)
+
