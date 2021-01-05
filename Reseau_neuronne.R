18f600d52eb95d5d0ab74282b2d9d3f246d6cb7e
Amélie <amelie.picard1@gmail.com>
2021-01-06 00:09:44 +0100

    ameliepcrd

--- Reseau_neuronne.R
+++ Reseau_neuronne.R
@@ -5,7 +5,6 @@ date: "26/12/2020"
 output: word_document
 ---
 
-
 ```{r}
 #chargement des donnees
 #setwd("C:/Users/ameli/Desktop/R/data_mining")
@@ -26,7 +25,11 @@ data$y = recode(data$y, "no" = 0, "yes" = 1)
 
 #Réalisation du réseau de neurones
 
-Nous allons réaliser un réseau de neurones à l'aide de la fonction "neutralnet" qui permet de réaliser tout type de réseau de neurones.
+Nous allons voir le protocole expérimentale, puis nous allons le décrire plus en détail le déroulement de l'analyse réalisé ainsi que les conclusions.
+
+##Protocole expérimental
+
+
 
 ##Transformation des données
 
@@ -91,11 +94,17 @@ yvalid = valid[, 17]
 
 
 ##Paramétrage du réseau de neurone
+
+Nous allons réaliser un réseau de neurones à l'aide de la fonction "neutralnet" qui permet de réaliser tout type de réseau de neurones.
 Nous allons paramétrer cette méthode grâce à de nombreux paramètres : 
  - hidden : spécifie le nombre de neuronnes dans la couche caché
  - err.fct : fonction utilisé pour determiner le calcul de l'erreur (ce : entropie croisée, sse : somme des erreurs au carré)
  - linear.output = FALSE : Ne pas réaliser de regression linéaire
- 
+ - algorithm : contient une chaine définissant le type d'algorithme, par défaut vaut "rprop+" qui signifie rétropropagation résiliente avec retour de poids
+ - act.fct : permet de lisser le résultat du produit croisé des neurones et des poids, par défaut "logistic" qui se rapporte à la fonction logistique.
+
+Cette fonction utilise d'autre paramètres comme : 
+ - startweights : contient la valeurs de départ des poids (par défaut une initialisation aléatoire)
  - rep : Nombre d'entrainement du réseau (pas utiliser pour éviter le sur-apprentissage)
 
 Dans le cas d'une variable y catégorielle binaire, nous choisissons comme fonction d'erreur l'entropie croissée.
@@ -109,14 +118,10 @@ nn$result.matrix
 
 ```
 
-Cette fonction utilise d'autre paramètres comme : 
- - startweights : contient la valeurs de départ des poids (par défaut une initialisation aléatoire)
-
 Validation du modèle : 
 
 ```{r}
-Predict=compute(nn,valid)
-prob <- Predict$net.result
+prob <- predict(nn,Xvalid)
 pred <- ifelse(prob>0.5, 1, 0)
 pred
 
@@ -131,12 +136,13 @@ tx<-(m[1,2])/length(yvalid)
 tx
 ```
 
+Le taux d'erreur de ce premier modéle est de 8,5%.
+
 Deuxième modèle : 
 ```{r}
 nn2=neuralnet(ytrain~., data = Xtrain, hidden=10, err.fct="ce",linear.output = FALSE,likelihood = TRUE)
 
-Predict2=compute(nn2,valid)
-prob2 <- Predict2$net.result
+prob2 <- predict(nn2,yvalid)
 pred2 <- ifelse(prob2>0.5, 1, 0)
 
 m2<-table(pred2,yvalid)
@@ -144,15 +150,15 @@ tx2<-(m2[1,2])/length(yvalid)
 tx2
 
 ```
-Modéle 2 moins performant avec le likelihood (taux d'erreur = 8% > 7%)
+Le modéle deux est plus performant avec le "likelihood" (taux d'erreur = 8,1% < 8,5%).
 
 Troixième modèle : augmentation du nombre de couche de la couche caché de 10 à 17 (nombre de variable d'entrée)
 ```{r}
 nn3=neuralnet(ytrain~., data = Xtrain, hidden=17, err.fct="ce",linear.output = FALSE)
 
-Predict3=compute(nn3,valid)
-prob3 <- Predict3$net.result
+prob3 <- predict(nn3,valid)
 pred3 <- ifelse(prob3>0.5, 1, 0)
+pred31<-pred3
 
 m3<-table(pred3,yvalid)
 tx3<-(m3[1,2])/length(yvalid)
@@ -160,14 +166,13 @@ tx3
 
 ```
 
-Taux d'erreur un peu meilleur (7,3%<7,5%)
+Taux d'erreur un peu meilleur (7,9%<8,1%)
 
 Quatrième modèle : augmentation du nombre de couche de la couche caché de 17 à 34 (deux fois le nombre de variable d'entrée)
 ```{r}
 nn4=neuralnet(ytrain~., data = Xtrain, hidden=34, err.fct="ce",linear.output = FALSE)
 
-Predict4=compute(nn4,valid)
-prob4 <- Predict4$net.result
+prob4 <- predict(nn4,valid)
 pred4 <- ifelse(prob4>0.5, 1, 0)
 
 m4<-table(pred4,yvalid)
@@ -176,26 +181,68 @@ tx4
 
 ```
 
-Taux d'erreur un peu élevé, donc moins intéressant (7,5%>7,3%)
+Taux d'erreur un peu élevé, donc moins intéressant (7,6%<7,9%)
 
-Nous décidons de garder 17 neurones dans la couche caché :
+On garde la prédiction n°3 pour le nombre de neurones de la couche caché. Et nous décidons de garder 17 neurones dans la couche caché.
 
+Cinquième modèle : On utilise une couche caché composé de 34 neurones et on passe l'argument "likelihood" à vrai.
+```{r}
+nn5<-neuralnet(ytrain~., data = Xtrain, hidden=34, err.fct="ce",linear.output = FALSE, likelihood = TRUE)
 
+prob5 <- predict(nn5,valid)
+pred5 <- ifelse(prob5>0.5, 1, 0)
+
+m5<-table(pred5,yvalid)
+tx5<-(m5[1,2])/length(yvalid)
+tx5
+
+```
+
+Cette fois ci le taux d'erreur est plus élevé (8,0%)
+
+Nous testons un dernier modéle.
+
+Sixième modèle : Nous augumentons encore une fois le nombre de neurones de la couche cachée (3 fois le nombre de variable d'entrée)
+```{r}
+nn6<-neuralnet(ytrain~., data = Xtrain, hidden=51, err.fct="ce",linear.output = FALSE, likelihood = TRUE)
+
+prob6 <- predict(nn6,valid)
+pred6 <- ifelse(prob6>0.5, 1, 0)
+
+m6<-table(pred6,yvalid)
+tx6<-(m6[1,2])/length(yvalid)
+tx6
+
+```
+Cette fois le taux d'erreur est de 7,8% ce qui est faiblement plus élevé que pour le quatrième modèle (7,6%). Mais ce modèle utilise un plus grand nombre de neurone ce qui peut engendrer du sur-apprentissage. 
+
+Avec la library "neutralnn" et la variable cible y correspondant à une classification binaire, il n'est pas possible de paramétrer le réseau de neurone à l'infinie. Les paramétrages pour une telle variable sont faible. Le paramètre "algorithme" ne fonctionne qu'avec "rprop +", la fonction d'erreur ne peut pas être modifier et la demande d'une régression logistique est impossible. Nous aurions pu changer les poids les variables d'entrée, mais le choix de l'aléatoire nous pariassait plus juste.
+Néanmoins, avec les réseaux de neurones que nous avons construit nous avons un taux d'erreur plutôt faible, ce qui nous paraissait être un bon résultat.Pour confirmer cette avis, nous décidons de comparer les courbes ROC des six modèles.
+
+courbe ROC : 
+```{r}
+all_pred = data.frame(pred, pred2, pred3, pred4, pred5, pred6)
+colnames(all_pred) = c("nn","nn2","nn3","nn4","nn5","nn6")
+colAUC(all_pred, yvalid, plotROC = TRUE)
+abline(0,1, col = "blue")
+
+```
+
+A l'aide de la Courbe ROC ainsi que du taux d'erreur, nous considérons le meilleur réseau de neurone étant le quatrième modèle construit. C'est a dire que nous utilisons 17 neuronnes caché et que nous plaçons à vrai l'argument "likelihood". C'est avec ce modèle que nous allons vérifier la bonne prédiction des valeurs test.
 
 ##Prédiction de la variable cible y
 
 Représentation graphique du réseau de neurones séléctionné : 
 
 ```{r}
-plot(nn)
+plot(nn4)
 
 ```
 
 Voici la distribution de la variable y prédite par le réseau de neurone :
 
 ```{r}
-Predict=compute(nn,test)
-prob <- Predict$net.result
+prob <- predict(nn4,test)
 pred <- ifelse(prob>0.5, 1, 0)
 pred
 
@@ -213,9 +260,15 @@ Voici le taux d'erreur de cette prédiction :
 tx<-(m[1,2])/length(ytest)
 tx
 ```
+Le taux d'erreur est de 7,1%.
 
+courbe ROC représentant : 
+```{r}
+colnames(pred)="Reseau de neurone"
+colAUC(pred, ytest, plotROC = TRUE)
+abline(0,1, col = "blue")
 
+```
 
-
-
+L'air sous la courbe représentant le reseau de neurones est supérieur à celle de la courbe représentant l'alétoire.
 
